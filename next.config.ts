cdf608f281c71ed6f7cbd92189f2073aea96e1ce
elkofix <holasoygermamwow@gmail.com>
2025-09-01 14:54:43 -0500

    feat: adding workflow for instant deploy to github pages

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    ignoreDuringBuilds: true, // ✅ Desactiva ESLint en "next build"
+    ignoreDuringBuilds: true,
   },
-  output: "export"
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
+  basePath: '/bioethics-hook-game', 
+  assetPrefix: '/bioethics-hook-game/'
 };
 
 export default nextConfig;
